ba518aef6f771720318e0d84f42d8f97d12b98af
CaffeinatedRat <caffeinatedrat@gmail.com>
2013-06-16 19:11:02 -0400

    Fixed all the tab spacing issues that were lost during transfer.

--- checkwebsite.py
+++ checkwebsite.py
@@ -27,60 +27,60 @@ Details: {5}
 
 class checkWebsiteThread (threading.Thread):
 
-        #Constructor...
-        def __init__(self, websiteName, websiteUrl, sleepTime=None):
-                threading.Thread.__init__(self)
-                self.websiteUrl = websiteUrl
-                self.websiteName = websiteName
-                self.isAlive = True
-                if sleepTime is None:
-                        self.sleepTime = 300
-                else:
-                        self.sleepTime = sleepTime
+	#Constructor...
+	def __init__(self, websiteName, websiteUrl, sleepTime=None):
+		threading.Thread.__init__(self)
+		self.websiteUrl = websiteUrl
+		self.websiteName = websiteName
+		self.isAlive = True
+		if sleepTime is None:
+			self.sleepTime = 300
+		else:
+			self.sleepTime = sleepTime
 
-        #Run method invoked when the thread starts.
-        def run(self):
-                while self.isAlive:
-                        self.checkWebsite(self.websiteName, self.websiteUrl)
-                        time.sleep(self.sleepTime)
-                print 'Site checking for site {0} has stopped.\n'.format(self.websiteUrl)
+	#Run method invoked when the thread starts.
+	def run(self):
+		while self.isAlive:
+			self.checkWebsite(self.websiteName, self.websiteUrl)
+			time.sleep(self.sleepTime)
+			print 'Site checking for site {0} has stopped.\n'.format(self.websiteUrl)
 
-        #Stops the threads internal loop.
-        def stop(self):
-                self.isAlive = False
+	#Stops the threads internal loop.
+	def stop(self):
+		self.isAlive = False
 
-        #Perform the actual check website logic.
-        def checkWebsite(self, websiteName, websiteUrl):
-                #Default to no error message.
-                errorMessage = None
+	#Perform the actual check website logic.
+	def checkWebsite(self, websiteName, websiteUrl):
+		#Default to no error message.
+		errorMessage = None
 
-                #Attempt to perform a head request on the website with time-out in seconds based on the timeOutVal
-                try:
-                        r = requests.head(websiteUrl, timeout=timeOutVal)
-                        r.raise_for_status()
-                except:
-                        e = sys.exc_info();
-                        errorMessage = str(e[0]) + '\r\n' + str(e[1])
+		#Attempt to perform a head request on the website with time-out in seconds based on the timeOutVal
+		try:
+			r = requests.head(websiteUrl, timeout=timeOutVal)
+			r.raise_for_status()
+		except:
+			e = sys.exc_info();
+			errorMessage = str(e[0]) + '\r\n' + str(e[1])
 
-                timeStamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+		timeStamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-                #An error has occured, attempt to email the issue to receiver.
-                if errorMessage != None:
-                        try:
-								compiledMessage = message.format(sender, receiver, websiteName, websiteUrl, timeStamp, errorMessage)
-                                smptObj = smtplib.SMTP('smtp.gmail.com:587')
-                                smptObj.starttls()
-                                smptObj.login('login', 'password')
-                                smptObj.sendmail(sender, receiver, compiledMessage)
-                                smptObj.quit()
-                                print 'The site {0} was reported down at {1} with the error {2}.\n'.format(websiteName, timeStamp, errorMessage)
-                        except:
-                                print sys.exc_info();
-                #Write to the console that everything is okay.
-                else:
-                        print 'The site {0} was reported good at {1}.\n'.format(websiteUrl, timeStamp)
+		#An error has occured, attempt to email the issue to receiver.
+		if errorMessage != None:
+			try:
+				compiledMessage = message.format(sender, receiver, websiteName, websiteUrl, timeStamp, errorMessage)
+				smptObj = smtplib.SMTP('smtp.gmail.com:587')
+				smptObj.starttls()
+				smptObj.login('login', 'password')
+				smptObj.sendmail(sender, receiver, compiledMessage)
+				smptObj.quit()
+				print 'The site {0} was reported down at {1} with the error {2}.\n'.format(websiteName, timeStamp, errorMessage)
+			except:
+				print sys.exc_info();
+		#Write to the console that everything is okay.
+		else:
+			print 'The site {0} was reported good at {1}.\n'.format(websiteUrl, timeStamp)
 
-                return
+		return
 
 #Main execution.
 mainSiteThread = checkWebsiteThread(webSites[0], webSites[1])
@@ -90,11 +90,11 @@ isAlive = True
 #Control loop.
 while isAlive:
 
-        #Get the user input.
-        input = raw_input('>> ').lower()
-        #Stop the control loop and shutdown the script.
-        if input == 'stop':
-                isAlive = False
+	#Get the user input.
+	input = raw_input('>> ').lower()
+	#Stop the control loop and shutdown the script.
+	if input == 'stop':
+		isAlive = False
 
 #Clean-up
 print '>>Stopping the script.\n'
